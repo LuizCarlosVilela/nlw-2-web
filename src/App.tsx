2da183b11a28dd3aece000a234c3f753e1d48a62
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-03 17:29:59 -0300

    Criando a página inicial totalmente responsiva

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import "./assets/styles/global.css";
+
+import Landing from './pages/Landing';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-
-    </div>
+    <>
+      <Landing />
+    </>
   );
 }
 
