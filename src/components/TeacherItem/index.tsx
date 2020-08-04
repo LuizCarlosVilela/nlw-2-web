f4823f5813bfa76675121c3616c8dd2c07f59c8f
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-03 22:43:17 -0300

    Completando tela de listagem de professores

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import './style.css';
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars3.githubusercontent.com/u/57350762?s=460&u=5c28607a6573c3f9ae703ebc9e23c31c916e3ab3&v=4" alt="Luiz Carlos" />
+                <div>
+                    <strong>Diego Fernandes</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+
+            <p>
+                Entusiastas das melhores tecnologias de química avançada.
+                        <br /><br />
+                        Apaixonado por explodir coisas em laboratório e mudar a vida das pessoas.
+                    </p>
+
+            <footer>
+                <p>
+                    Preço/Hora
+                            <strong>R$ 20,00</strong>
+                </p>
+
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp" />
+                            Entrar em contato
+                        </button>
+            </footer>
+
+        </article>
+
+    );
+}
+
+export default TeacherItem;
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 function TeacherForm (){
     return (
-        <h1>Cadastrar Teacher</h1>
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que incrível que você quer dar aulas"/>
+        </div>
     )
 }
 
