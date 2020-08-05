8683a2c7cc2163ef26c8f7361a22305858bd449a
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-05 14:23:01 -0300

    Fazendo formulário de cadastro, TeacherForm no styled

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
-import PageHeader from '../../components/PageHeader';
 
-function TeacherForm (){
+import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
+
+function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrível que você quer dar aulas"/>
+            <PageHeader
+                title="Que incrível que você quer dar aulas"
+                description="O primeiro passo é preencher esse formulário de inscrição"
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <Input label="Nome completo" name="name" />
+                    <Input label="Avatar" name="avatar" />
+                    <Input label="WhatsApp" name="whatsapp" />
+
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Input label="Matéria" name="subject" />
+                    <Input label="Custo da sua hora por aula" name="cost" />
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante" />
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
+
+            </main>
         </div>
     )
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 import './styles.css';
 
@@ -12,20 +12,9 @@ function TeacherList() {
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
 
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Input label="Matéria" name="subject" />
+                    <Input label="Dia da Semana" name="week_day" />
+                    <Input  type="time" label="Hora" name="time" />
 
                 </form>
             </PageHeader>
